03b462fd7429ad0464c2cc04571d5ff72b166eea
ahmdRmaa <158021611+ahmdRmaa@users.noreply.github.com>
2025-01-02 22:03:28 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <img src="~/assets/LOGO-SMK4.png" style="width:3%;" alt="">
+    <a class="navbar-brand" href="#">SMK NEGERI 4 Tasikmalaya</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Jurusan</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Tentang
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Visi & Misi</a></li>
+            <li><a class="dropdown-item" href="#">Sejarah</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Galeri6666</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Ekstrakulikuler</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid">
+    <div class="row d-flex justify-content-center">
+      <div class="col-10 p-5 ">
+        <div class="card p-5 rounded-5">
+          <div class="row">
+            <div class="col-6 ">
+              <img src="~/assets/kepsek.png" style="width:100%;" alt="">
+            </div>
+            <div class="col-6">
+            Assalamualaikum Warahmatullahi Wabarakatuh,  
+              segala puji dan syukur kita panjatkan kehadirat allah SWT, semoga kita semua ada dalam lindungannya dan karunianya
+              , kami berharap dengan adanya website SMK Negeri 4 Tasikmalaya ini para pengunjung dapat mengenal lebih jauh tentang
+              sekolah kami sehingga dapat pempererat tali silaturahmi antara sekolah dengan masyarakat demi kemajuan kita bersama. 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
